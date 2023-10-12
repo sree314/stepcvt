291cf1d9608f0ced2069c6dd52bab93b18ea4f15
Sreepathi Pai <sree@cs.rochester.edu>
2023-10-12 15:59:57 -0400

    Add skeletons of project and storage

---
+++ stepcvt/project.py
@@ -0,0 +1,19 @@
+# Contains classes for Project information
+#
+# The top-level object is a Project, which can contain multiple Source
+# files, with each Source file containing information on individal
+# part. Each PartInfo contains a set of information, each for a
+# specific task. Right now, only the STLConversionTask is specified.
+
+
+class Project:
+    pass
+
+class Source:
+    pass
+
+class PartInfo:
+    pass
+
+class STLConversionInfo:
+    pass
---
+++ stepcvt/storage.py
@@ -0,0 +1,21 @@
+# Classes to assist in loading and saving project information to files
+
+class ReaderWriter:
+    def read(self, filename):
+        """Read a file and generate a project"""
+
+        raise NotImplementedError
+
+    def write(self, project, filename):
+        """Write out file for a project"""
+
+        raise NotImplementedError
+
+
+class JSONReaderWriter(ReaderWriter):
+    pass
+
+
+class YAMLReaderWriter(ReaderWriter):
+    pass
+
