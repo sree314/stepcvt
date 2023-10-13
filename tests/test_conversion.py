4852ca8f5fd0c3279f1421b7cd08ed5b000c24b4
sree <sree@buckyball>
2023-10-13 10:13:37 -0400

    Add from_dict test

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -4,3 +4,17 @@ def test_STLConversionInfo_to_dict():
     x = STLConversionInfo()
     d = x.to_dict()
     assert isinstance(d, dict)
+
+def test_STLConversionInfo_from_dict():
+    si_info = {
+        'rotation': [0,0,0],
+        'linearTolerance': 0.1,
+        'angularTolerance': 0.1,
+        }
+
+    si = STLConversionInfo.from_dict(si_info)
+    assert isinstance(si, STLConversionInfo)
+    assert si.rotation == [0,0,0]
+    assert si.linearTolerance == 0.1
+    assert si.angularTolerance == 0.1
+
