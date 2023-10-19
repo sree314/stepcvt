803581061e261ca50e0a763c92e2e480af35dd50
Sreepathi Pai <sree@cs.rochester.edu>
2023-10-19 13:01:15 -0400

    Add test for STLConversionInfo __init__

--- tests/test_stl_conversion.py
+++ tests/test_stl_conversion.py
@@ -2,10 +2,15 @@ from stepcvt.project import STLConversionInfo
 
 
 def test_STLConversionInfo_to_dict():
-    x = STLConversionInfo()
+    x = STLConversionInfo(
+        rotation=[0, 90, 0], linearTolerance=0.1, angularTolerance=0.15
+    )
     d = x.to_dict()
     assert isinstance(d, dict)
     assert d["type"] == "STLConversionInfo"
+    assert d["rotation"] == [0, 90, 0]
+    assert d["linearTolerance"] == 0.1
+    assert d["angularTolerance"] == 0.15
 
 
 def test_STLConversionInfo_from_dict():
