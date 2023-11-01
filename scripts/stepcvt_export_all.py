8762bb5d230b10bf489cf0c6878d31ec83c3c4f6
yumenghe <yhe54@u.rochester.edu>
2023-10-31 22:49:35 -0400

    add support to limit parts exported and custom rotations

--- scripts/stepcvt_export_all.py
+++ scripts/stepcvt_export_all.py
@@ -17,6 +17,8 @@
 import argparse
 from pathlib import Path
 from stepcvt.project import Project
+import sys
+import re
 
 
 if __name__ == "__main__":
@@ -28,11 +30,26 @@ if __name__ == "__main__":
         "output_directory",
         help="Output directory where step files will be placed (e.g. /tmp)",
     )
+    p.add_argument(
+        "--partid", 
+        help="Specify part to be exported"
+    )
+    p.add_argument(
+        "--rot", 
+        help="Apply custom rotation to the specified part ('x,y,z' in degrees,"
+            " only support integers)"
+    )
+    p.add_argument(
+        "--exclude",
+        nargs="*",
+        help="Export all objects except those specified here with partid"
+    )
 
     args = p.parse_args()
 
     stepfile = Path(args.stepfile)
     odir = Path(args.output_directory)
+    exclude_ids = args.exclude  # None if not specified
 
     # create a project and load the step file
     p = Project(name=stepfile.name)
@@ -41,17 +58,61 @@ if __name__ == "__main__":
     # go through all parts in the step file and create partinfo for
     # them
     source = p.sources[0]
-    for partid, obj in source.parts():
-        source.add_partinfo(partid, obj)
+        
+    if exclude_ids != None:
+        for partid, obj in source.parts():
+            if partid not in exclude_ids:
+                source.add_partinfo(partid, obj)
+    else:
+        specified_ids = []
+        specified_rot = []
+        # get arguments for (partid + rot)'s and parse them out
+        argstr = ' '.join(sys.argv[3:])
+        pattern = r'--partid\s(?P<id>\S*)(\s)?(--rot\s(?P<x>[-+]?\d+),(?P<y>[-+]?\d+),(?P<z>[-+]?\d+)(\s)?)?'
+        matches = re.finditer(pattern, argstr)
+        for match in matches:
+            partid = match.group("id")
+            rot = (match.group("x"), match.group("y"), match.group("z"))
+            specified_ids.append(partid)
+            specified_rot.append(rot)
+            
+        # print(specified_ids)
+        # print(specified_rot)
 
+        # export all parts, i.e. no partid and exclude specified
+        if not specified_ids: 
+            for partid, obj in source.parts():
+                source.add_partinfo(partid, obj)
+        else:
+            for sid in specified_ids:  # keep the same order with specified_rot
+                for partid, obj in source.parts():
+                    if sid == partid:
+                        source.add_partinfo(partid, obj)
+    
+
+    # user has specified some rotations for specific parts
+    # there might be the case that specified_ids is not empty and specified_rot is empty
+    if specified_rot:
+        for pi, rot in zip(source.partinfo, specified_rot):
+            pi.add_info(
+                stepcvt.project.STLConversionInfo(
+                    rotation=[int(rot[0]) if rot[0] is not None else 90, 
+                              int(rot[1]) if rot[1] is not None else 0,  
+                              int(rot[2]) if rot[2] is not None else 0], 
+                    linearTolerance=0.1, 
+                    angularTolerance=0.2
+                )
+            )
+    
     # go through each added partinfo and add some STLConversionInfo
     # with some arbitrary rotation for now
-    for pi in source.partinfo:
-        pi.add_info(
-            stepcvt.project.STLConversionInfo(
-                rotation=[90, 0, 0], linearTolerance=0.1, angularTolerance=0.2
+    else:
+        for pi in source.partinfo:
+            pi.add_info(
+                stepcvt.project.STLConversionInfo(
+                    rotation=[90, 0, 0], linearTolerance=0.1, angularTolerance=0.2
+                )
             )
-        )
 
     # finally, export each object to a step file. For now, assume the
     # object's partid is a valid filename too.
