cd5a1e80ed3af2c3a5b563f176c5d16b1f10c8ae
yumenghe <yhe54@u.rochester.edu>
2023-11-01 12:11:23 -0400

    replace reparsing

--- scripts/stepcvt_export_all.py
+++ scripts/stepcvt_export_all.py
@@ -14,10 +14,15 @@
 #
 # Download it someplace and pass it as an argument to this script
 
+# Example: stepcvt_export_all.py file.step out_dir --partid part1 --rot 0,45,0 --partid part2 --rot 90,0,0
+# Example: stepcvt_export_all.py file.step out_dir --exclude part1 part2
+# --partid needs to be followed by --rot
+# --rot 0,0,0 corresponds to no rotation
+# --rot aruguments only support positive degrees
+
 import argparse
 from pathlib import Path
 from stepcvt.project import Project
-import sys
 import re
 
 
@@ -32,16 +37,18 @@ if __name__ == "__main__":
     )
     p.add_argument(
         "--partid", 
+        action="append",
         help="Specify part to be exported"
     )
     p.add_argument(
         "--rot", 
+        action="append",
         help="Apply custom rotation to the specified part ('x,y,z' in degrees,"
             " only support integers)"
     )
     p.add_argument(
         "--exclude",
-        nargs="*",
+        nargs="+",
         help="Export all objects except those specified here with partid"
     )
 
@@ -49,8 +56,10 @@ if __name__ == "__main__":
 
     stepfile = Path(args.stepfile)
     odir = Path(args.output_directory)
-    exclude_ids = args.exclude  # None if not specified
-
+    specified_ids = args.partid
+    specified_rot = args.rot
+    exclude_ids = args.exclude
+    
     # create a project and load the step file
     p = Project(name=stepfile.name)
     p.add_source(name=stepfile.name, path=stepfile)
@@ -60,45 +69,34 @@ if __name__ == "__main__":
     source = p.sources[0]
         
     if exclude_ids != None:
+        exclude_ids = set(exclude_ids)
         for partid, obj in source.parts():
             if partid not in exclude_ids:
                 source.add_partinfo(partid, obj)
+    # export all parts, i.e. no partid and exclude specified
+    elif specified_ids is None: 
+        for partid, obj in source.parts():
+            source.add_partinfo(partid, obj)
     else:
-        specified_ids = []
-        specified_rot = []
-        # get arguments for (partid + rot)'s and parse them out
-        argstr = ' '.join(sys.argv[3:])
-        pattern = r'--partid\s(?P<id>\S*)(\s)?(--rot\s(?P<x>[-+]?\d+),(?P<y>[-+]?\d+),(?P<z>[-+]?\d+)(\s)?)?'
-        matches = re.finditer(pattern, argstr)
-        for match in matches:
-            partid = match.group("id")
-            rot = (match.group("x"), match.group("y"), match.group("z"))
-            specified_ids.append(partid)
-            specified_rot.append(rot)
-            
-        # print(specified_ids)
-        # print(specified_rot)
-
-        # export all parts, i.e. no partid and exclude specified
-        if not specified_ids: 
-            for partid, obj in source.parts():
+        dict_id_rot = {}
+        for id, rot in zip(specified_ids, specified_rot):
+            pattern = r"(?P<x>\d+),(?P<y>\d+),(?P<z>\d+)"
+            m = re.match(pattern, rot)
+            dict_id_rot.__setitem__(id, [int(m.group('x')),
+                                        int(m.group('y')),
+                                        int(m.group('z'))])
+        
+        specified_ids = set(specified_ids)
+        for partid, obj in source.parts():
+            if partid in specified_ids:
                 source.add_partinfo(partid, obj)
-        else:
-            for sid in specified_ids:  # keep the same order with specified_rot
-                for partid, obj in source.parts():
-                    if sid == partid:
-                        source.add_partinfo(partid, obj)
-    
 
-    # user has specified some rotations for specific parts
-    # there might be the case that specified_ids is not empty and specified_rot is empty
-    if specified_rot:
-        for pi, rot in zip(source.partinfo, specified_rot):
+    # add custom/random STLConversionInfo to parts
+    if specified_ids is not None:
+        for pi in source.partinfo:
             pi.add_info(
                 stepcvt.project.STLConversionInfo(
-                    rotation=[int(rot[0]) if rot[0] is not None else 90, 
-                              int(rot[1]) if rot[1] is not None else 0,  
-                              int(rot[2]) if rot[2] is not None else 0], 
+                    rotation=dict_id_rot[pi.part_id],
                     linearTolerance=0.1, 
                     angularTolerance=0.2
                 )
