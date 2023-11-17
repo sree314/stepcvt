b3c81a139b92ca205e0832fdaac3c34f60c5cab9
Asai-Yume <624156437k@gmail.com>
2023-11-17 10:00:05 -0500

    closes 27; fixed failing tests

--- tests/test_cadsource.py
+++ tests/test_cadsource.py
@@ -194,6 +194,7 @@ def test_CADSource_add_partinfo(tmp_path):
     cs = CADSource.load_step_file("book", model_file)
 
     for partid, obj in cs.parts():
+        print(partid, obj)
         pi = cs.add_partinfo(partid, obj)
 
         assert isinstance(pi, PartInfo)
