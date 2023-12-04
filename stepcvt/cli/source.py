fa8d92fd68177efa08d9c6661838f17ad4d8dfef
Asai-Yume <624156437k@gmail.com>
2023-12-03 23:21:13 -0500

    update source.py

--- stepcvt/cli/source.py
+++ stepcvt/cli/source.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from ..project import *
 
 
-def add_step(args):
+def add_step(p, args):
     if args.jsonfile is None:
         print("ERROR: Need to provide a jsonfile")
         return 1
@@ -14,6 +14,7 @@ def add_step(args):
         print(f"Adding step file: {args.step_path} to {args.jsonfile}.")
         stepfile = Path(args.step_path)
         cs = CADSource(name="test", path=stepfile)
+        p.add_source(cs, args.step_name, args.step_path)
         cs.load_step_file()
         try:
             with open(args.jsonfile, "w") as j:
@@ -27,20 +28,16 @@ def add_step(args):
             return 1
 
 
-def remove_step(args):
+def remove_step(p, args):
     # TODO Needs to be implemented
     print(f"Removing step file: {args.step_path} from {args.project}")
 
 
-def list_parts(args):
-    # if args.jsonfile is None:
-    #     print("ERROR: Need to provide a jsonfile")
-    #     return 1
-
+def list_parts(p, args):
     if args.step_path is None:
         print("Missing path to step file")
     else:
-        print(f"Listing parts of step file: {args.name}")
+        print(f"Listing parts of step file: {args.step_name}")
         stepfile = Path(args.step_path)
         cs = CADSource(name="temp", path=stepfile)
         for partid, _ in cs.parts():
