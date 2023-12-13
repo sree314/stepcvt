a645e1877a7007ecc70d880f1749c8901c50eff1
Adira Blumenthal <adiraeb@gmail.com>
2023-12-13 09:09:46 -0500

    change python to python3

--- stepcvt/cli/cli_test.py
+++ stepcvt/cli/cli_test.py
@@ -24,21 +24,21 @@ def test_command(command, test_name):
 
 # defining the tests
 tests = {
-    "Project Test 1": "python ./scripts/stepcvt -j json.js make",  # making the project
-    "Project Test 2": "python ./scripts/stepcvt -j json.js newProjName test",  # renaming the project from 'stepcvt' to 'test'
-    "Project Test 3": "python ./scripts/stepcvt -j json.js display",  # show the projects contents so far
-    "source add": "python ./scripts/stepcvt -j json.js addstep stealth_burner_head scripts/Stealthburner_Printhead_V6.stp",  # adding the source
-    "source list": "python ./scripts/stepcvt -j json.js liststep stealth_burner_head",  # lising step parts
-    "source remove": "python ./scripts/stepcvt -j json.js rmstep stealth_burner_head",  # removing the source
-    "source add part": "python ./scripts/stepcvt -j json.js addpart stealth_burner_head --all",  # adding all the parts to the first source
-    "stl test": "python ./scripts/stepcvt -j json.js stlconvert partID --rotation 0 0 0 --linearTolerance 0.1 --angularTolerance 0.1",  # adding angular/linear tolerances
-    "export test": "python ./scripts/stepcvt -j json.js exportstl path",  # exporting the stls
-    "choices add test": "python ./scripts/stepcvt -j json.js choices add-chooser --type single 'Printer Options' 'options' "
+    "Project Test 1": "python3 ./scripts/stepcvt -j json.js make",  # making the project
+    "Project Test 2": "python3 ./scripts/stepcvt -j json.js newProjName test",  # renaming the project from 'stepcvt' to 'test'
+    "Project Test 3": "python3 ./scripts/stepcvt -j json.js display",  # show the projects contents so far
+    "source add": "python3 ./scripts/stepcvt -j json.js addstep stealth_burner_head scripts/Stealthburner_Printhead_V6.stp",  # adding the source
+    "source list": "python3 ./scripts/stepcvt -j json.js liststep stealth_burner_head",  # lising step parts
+    "source remove": "python3 ./scripts/stepcvt -j json.js rmstep stealth_burner_head",  # removing the source
+    "source add part": "python3 ./scripts/stepcvt -j json.js addpart stealth_burner_head --all",  # adding all the parts to the first source
+    "stl test": "python3 ./scripts/stepcvt -j json.js stlconvert partID --rotation 0 0 0 --linearTolerance 0.1 --angularTolerance 0.1",  # adding angular/linear tolerances
+    "export test": "python3 ./scripts/stepcvt -j json.js exportstl path",  # exporting the stls
+    "choices add test": "python3 ./scripts/stepcvt -j json.js choices add-chooser --type single 'Printer Options' 'options' "
     "'HEPA filter':'Filter' 'Build area lights':'Lights':'version=='V6''",  # Add a single chooser
-    "choices edit test": "python ./scripts/stepcvt -j json.js choices edit 'options' --choice-value 'Lights' "
+    "choices edit test": "python3 ./scripts/stepcvt -j json.js choices edit 'options' --choice-value 'Lights' "
     "'Build area lights':'Lights':'version=='V4''",  # rename one of the choice value
-    "choices remove test": "python ./scripts/stepcvt -j json.js choices remove 'options' --choice-value 'Lights' --cond",  # remove one option value
-    "choices apply test": "python ./scripts/stepcvt -j json.js choices apply options=Lights,Filter",  # apply a user choice
+    "choices remove test": "python3 ./scripts/stepcvt -j json.js choices remove 'options' --choice-value 'Lights' --cond",  # remove one option value
+    "choices apply test": "python3 ./scripts/stepcvt -j json.js choices apply options=Lights,Filter",  # apply a user choice
 }
 
 all_tests_passed = True
